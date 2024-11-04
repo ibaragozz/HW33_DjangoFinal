b37ffeec667ef63b2e53b47cee879a7724ce2e3d
ibaragozz <baragozz1@gmail.com>
2024-11-04 11:55:40 +0300

    almoust done

--- movie_project/films/views.py
+++ movie_project/films/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Film
+from .forms import FilmForm
 
-# Create your views here.
+# Страница для добавления фильма
+def add_film(request):
+    if request.method == 'POST':
+        form = FilmForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('film_list')
+    else:
+        form = FilmForm()
+    return render(request, 'add_film.html', {'form': form})
+
+# Страница для просмотра фильмов
+def film_list(request):
+    films = Film.objects.all()
+    return render(request, 'film_list.html', {'films': films})
+
